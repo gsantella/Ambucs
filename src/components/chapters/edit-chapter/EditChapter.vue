d1b8c02a1b5eb468dc7e86a964fd04aec58dc687
tdavis81 <35125581+tdavis81@users.noreply.github.com>
2019-03-09 20:18:21 -0500

    Added Chapter Edit/Add Page

--- src/components/chapters/edit-chapter/EditChapter.vue
+++ src/components/chapters/edit-chapter/EditChapter.vue
@@ -1,11 +1,77 @@
 <template>
-<div>
+  <div class="form-elements">
+    <div class="va-row">
+      <div class="flex md12">
+        <vuestic-widget :headerText="'Edit Chapter' | translate">
+          <form>
+            <fieldset>
+              <div class="form-group">
+                <!-- City -->
+                <div class="input-group">
+                  <input id="simple-input1" type="text" required v-model="city" autocomplete="off"/>
+                  <label class="control-label" for="simple-input1" >City</label><i class="bar"></i>
+                </div>
 
-</div>
+                <!-- State -->
+                <div class="input-group">
+                  <input id="simple-input" type="text" required v-model="state" autocomplete="off" />
+                  <label class="control-label" for="simple-input">State</label><i class="bar"></i>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <!-- Code -->
+                <div class="input-group">
+                  <input id="simple-input1" type="text" required v-model="code" autocomplete="off"/>
+                  <label class="control-label" for="simple-input1">Code</label><i class="bar"></i>
+                </div>
+              </div>
+
+            </fieldset>
+
+        </form>
+        </vuestic-widget>
+
+      </div>
+    </div>
+     <!--Save Delete Buttons -->
+    <div class="va-row btn-margin-row">
+      <div
+        class="flex sm6 lg6 xl3 justify--center">
+        <button class="btn btn-primary" @click="updateChapter()" >
+          {{'Update' | translate}}
+        </button>
+      </div>
+      <div
+        class="flex sm6 lg6 xl3 justify--center">
+        <button class="btn btn-danger" @click="deleteChapter()">
+          {{'Delete' | translate}}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'EditChapter'
+  name: 'EditChapter',
+  date () {
+    return {
+      city: '',
+      state: '',
+      code: ''
+    }
+  },
+  methods: {
+    updateChapter () {
+
+    },
+    deleteChapter () {
+
+    }
+  },
+  created () {
+
+  }
 }
 </script>
